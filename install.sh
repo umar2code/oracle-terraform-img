649995ae9eb1e8bf34cd362d3f811026b028c49e
Mahesh <maheshg@sysgain.com>
2018-08-30 16:12:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 curl -o soln.zip $TEMPLATE_URL
 curl -o bmcs_api_key.pem $PRIVATE_KEY_URL
-v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
-v="$v."
-cp -r $v .
+unzip -d . soln.zip
